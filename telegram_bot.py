2e3454095c9cf9f5a9c6bcdd913f60b707a40848
LuisGS <luis.ild@gmail.com>
2021-10-27 01:08:38 +0200

    Bot nows return Netatmo room temp values

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,22 +1,26 @@
+import time
+import random
+import datetime
+import telepot
+from telepot.loop import MessageLoop
 import sys, logging
+
 # Logging as output logging messages.
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
 # all our PERSONAL variables are stored in here
 import variables
 
+# RPI temperatures functions
+error_import = False
 try:
+    error_import = True
     from humidity import temp
-    list_temp = temp()
-except ImportError:
-    list_temp = False
-    logging.exception("Import Adafruit_DHT has failed!")
+except (ModuleNotFoundError, ImportError) as error:
+    logging.exception("Import Adafruit_DHT has failed! %s", error)
 
-import time
-import random
-import datetime
-import telepot
-from telepot.loop import MessageLoop
+import temp_netatmo
+
 
 """
 """
@@ -26,23 +30,27 @@ def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
 
-    logging.INFO('Got command: %s' % command)
+    logging.info('Got command: %s' % command)
 
     if command == '/roll':
         bot.sendMessage(chat_id, random.randint(1,6))
     elif command == '/time':
         bot.sendMessage(chat_id, str(datetime.datetime.now()))
     elif command == '/temp':
-        if list_temp:
-            bot.sendMessage(chat_id, "Here you have your temps and humidity values!")
+        bot.sendMessage(chat_id, "Here you have your temps and humidity values!")
+        # RPis temps
+        if not error_import:
+            list_temp = temp()
             temp_1="Sensor 1: {0:0.1f}*C%".format(list_temp[0][0])
             temp_2="Sensor 2: {0:0.1f}*C%".format(list_temp[1][0])
             bot.sendMessage(chat_id, temp_1)
             bot.sendMessage(chat_id, temp_2)
         else:
             bot.sendMessage(chat_id, "RPI's sensors are not responding!")
-            logging.error("RPI's sensors are not responding.")
+            logging.info("RPI's sensors are not responding.")
 
+        # Netatmo temps!
+        bot.sendMessage(chat_id, "NETATMO: " + str(temp_netatmo.netatmo_room_temp()))
 
     else:
         bot.sendMessage(chat_id, "Sorry, I did not quite understand...")
@@ -52,6 +60,7 @@ bot = telepot.Bot(variables.token_bot_api)
 
 # Welcome message! we just started up!
 bot.sendMessage(variables.bot_chat_id, "Hi there! Im all awake!")
+
 MessageLoop(bot, handle).run_as_thread()
 logging.info('I am listening ...')
 
--- temp_netatmo.py
+++ temp_netatmo.py
@@ -2,10 +2,13 @@ import requests
 import time
 import json
 import variables
+import sys, logging
 
+# Logging as output logging messages.
+logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
-
-
+# Client credentials grant type
+# https://api.netatmo.com/oauth2/token
 def netatmo_grant_token():
     data = dict(grant_type = 'password', client_id = variables.netatmo_client_id,
             client_secret = variables.netatmo_client_secret,
@@ -15,21 +18,24 @@ def netatmo_grant_token():
                             verify=False)   # Ignore SSL certificate's verification
     return resp
 
-
+# Get current status of a home and devices
+# https://api.netatmo.com/api/homesdata
 def netatmo_homestatus():
     resp = netatmo_grant_token()
     if resp.status_code == 200:
         token = resp.json()
         token['expiry'] = int(time.time()) + token['expires_in']
-        print("TOKEN: " + token['access_token'])
+        logging.debug("TOKEN: " + token['access_token'])
         resp = requests.get('https://api.netatmo.com/api/homesdata?access_token=' +
                                 token['access_token'], verify=False)
         data = resp.json()
-        print(json.dumps(data))
+        #print(json.dumps(data))
+        return json.dumps(data)
     else:
-        print("There is an error")
+        logging.info("There is an error")
 
-
+# retrieve data of a devices
+# https://api.netatmo.com/api/homestatus
 def netatmo_room_temp():
     resp = netatmo_grant_token()
     if resp.status_code == 200:
@@ -39,8 +45,8 @@ def netatmo_room_temp():
         params = {'home_id' : variables.netatmo_home_id}
         temp_int = requests.get('https://api.netatmo.com/api/homestatus', headers=netatmo_headers, params=params, verify=False).json()
         room_temp = temp_int['body']['home']['rooms'][0]['therm_measured_temperature']
-        print(room_temp)
+        return room_temp
     else:
-        print("There is an error")
+        logging.info("There is an error")
 
-netatmo_room_temp()
+logging.info("Netatmo: room Temp is: " + str(netatmo_room_temp()))
