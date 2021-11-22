78f255054a032fb5944debd6d4e8895446c47bb7
LuisGS <luis.ild@gmail.com>
2021-11-22 22:19:46 +0100

    take pictures is fixed now. filename is corrected

--- take_picture.py
+++ take_picture.py
@@ -1,17 +1,18 @@
 # import libraries
-import time
+from datetime import datetime
 import os
 
 download_dir = "/home/osmc/Pictures/webcam/"
 
 def take_1_pic():
-    path_to_pic = download_dir +'%d%m%y_%H%M%S.jpg'
-    os.system('fswebcam -S 100 --save ' + path_to_pic)
+    now = datetime.now()
+    path_to_pic = download_dir + now.strftime("%d%m%y_%H%M%S") + ".jpg"
+    os.system('fswebcam -r 640x480 -S 100 --save ' + path_to_pic)
     return path_to_pic
 
 
 def path_to_pic():
-    return take_1_pic
+    return take_1_pic()
 
 # testing
 # take_1_pic()
