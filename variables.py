38848d6dadb5b0165b869eb65dfefd25e6c0268c
LuisGS <luis.ild@gmail.com>
2021-11-22 20:51:39 +0100

    file with our variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+"""
+
+Telegram BoT's Variables File
+
+This Files contains all relevant variables
+for our Telegram bot
+
+
+"""
+# Telegram BoT variables
+bot_chat_id = 1234
+token_bot_api = 'token_api'
+
+
+# netatmo
+netatmo_client_id = 'netatmo_client_id'
+netatmo_client_secret = 'netatmo_client_secret'
+netatmo_username = 'email'
+netatmo_password = 'passwordd'
+netatmo_home_id = 'home_id'
